46c1276b6d4cbe4320a651c54d383903c6a20bb7
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-12 04:19:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Write your solution here!
 
-  const pets = ["Milo", "Otis", "Garfield"]
-
-  let append = pets.push("Odie");
+  const append = ["Milo", "Otis", "Garfield"]
+  append.push("Odie");
 
   let prepend = pets.unshift("Odie");
