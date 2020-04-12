332da027b4d066e8f887d8c61d33bd7aa52b0d6a
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-12 04:14:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Write your solution here!
- const array_functions = ["Milo", "Otis", "Garfield"];
+ const array functions = ["Milo", "Otis", "Garfield"];
 
  let append = array_functions.push("Odie");
+
+ let 
+ 
